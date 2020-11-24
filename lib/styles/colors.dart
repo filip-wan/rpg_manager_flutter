1837d972aa689383e08ff3aef4a04399de6fefee
Filip Wanat <filip.k.wanat@gmail.com>
2020-11-24 23:52:07 +0100

    💄 Add theme file

---
+++ lib/styles/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+Map<String, Color> colors = {
+  "background": Color(0xff444444), // #444
+  "navbar": Color(0xff232323), // #232323
+  "text": Color(0xffe2e2e2), // #e2e2e2
+  "button": Color(0xff636363), // #636363
+};
---
+++ lib/styles/theme.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:rpg_manager_flutter/styles/colors.dart';
+
+ThemeData buildThemeData() => ThemeData(
+      visualDensity: VisualDensity.adaptivePlatformDensity,
+      appBarTheme: AppBarTheme(
+        color: colors["navbar"],
+        brightness: Brightness.dark,
+      ),
+      buttonTheme: ButtonThemeData(
+        buttonColor: Colors.deepPurple,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 10.0),
+      ),
+      textTheme: TextTheme(
+          headline1: TextStyle(
+              fontSize: 100,
+              fontWeight: FontWeight.normal,
+              color: colors["text"]),
+          headline2: TextStyle(
+              fontSize: 60,
+              fontWeight: FontWeight.normal,
+              color: colors["text"]),
+          button: TextStyle(
+            fontSize: 50,
+            fontWeight: FontWeight.normal,
+          )),
+      brightness: Brightness.dark,
+      backgroundColor: colors["background"],
+      primaryColor: colors["background"],
+      accentColor: colors["text"],
+      canvasColor: colors["background"],
+      buttonColor: colors["button"],
+    );
