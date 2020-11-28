f90c87b211edd297a8c45baac32f73c3f95599cf
Wojtek <vopox07@gmail.com>
2020-11-29 00:47:04 +0100

    Fix action form overflow
    Fixed by removing most of the padding and adding ScrollView

--- lib/widgets/action_form.dart
+++ lib/widgets/action_form.dart
@@ -11,76 +11,79 @@ class ActionForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      contentPadding: EdgeInsets.fromLTRB(18, 10, 18, 10),
+      contentPadding: EdgeInsets.fromLTRB(15, 5, 15, 5),
       content: Stack(
         overflow: Overflow.visible,
         children: <Widget>[
-          Positioned(
-            right: -15,
-            top: -15,
-            child: CloseButton(),
-          ),
           Form(
             key: _formKey,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.zero,
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                      hintText: 'What is this action\'s name?',
-                      labelText: 'Action Name',
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.only(top: 5),
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: 'What is this action\'s name?',
+                        labelText: 'Action Name',
+                      ),
+                      validator: (value) => value.isEmpty
+                          ? 'You have to enter Action Name'
+                          : null,
+                      onSaved: (String value) {
+                        model.name = value;
+                      },
                     ),
-                    validator: (value) =>
-                        value.isEmpty ? 'You have to enter Action Name' : null,
-                    onSaved: (String value) {
-                      model.name = value;
-                    },
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 8),
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                      hintText: 'What does this action do?',
-                      labelText: 'Description',
+                  Container(
+                    padding: EdgeInsets.only(top: 5),
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: 'What does this action do?',
+                        labelText: 'Description',
+                      ),
+                      onSaved: (String value) {
+                        model.description = value;
+                      },
                     ),
-                    onSaved: (String value) {
-                      model.description = value;
-                    },
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 8),
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                      hintText: 'What happens on dice throw?',
-                      labelText: 'Dice Equation',
+                  Container(
+                    padding: EdgeInsets.only(top: 5),
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: 'What happens on dice throw?',
+                        labelText: 'Dice Equation',
+                      ),
+                      validator: (value) => value.isEmpty
+                          ? 'You have to enter Dice Equation'
+                          : null,
+                      onSaved: (String value) {
+                        model.diceEquation = value;
+                      },
                     ),
-                    validator: (value) => value.isEmpty
-                        ? 'You have to enter Dice Equation'
-                        : null,
-                    onSaved: (String value) {
-                      model.diceEquation = value;
-                    },
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 10),
-                  child: RaisedButton(
-                    child: Text("Add Action"),
-                    onPressed: () {
-                      if (_formKey.currentState.validate()) {
-                        _formKey.currentState.save();
-                        onSaved(this.model);
-                        Navigator.of(context).pop();
-                      }
-                    },
-                  ),
-                )
-              ],
+                  Container(
+                    padding: const EdgeInsets.only(top: 10),
+                    child: RaisedButton(
+                      child: Text("Add Action"),
+                      onPressed: () {
+                        if (_formKey.currentState.validate()) {
+                          _formKey.currentState.save();
+                          onSaved(this.model);
+                          Navigator.of(context).pop();
+                        }
+                      },
+                    ),
+                  )
+                ],
+              ),
             ),
+          ),
+          Positioned(
+            right: -10,
+            top: -5,
+            child: CloseButton(),
           ),
         ],
       ),
